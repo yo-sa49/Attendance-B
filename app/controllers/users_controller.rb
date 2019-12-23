62fd10f713d08b1c8129ea0e5d631d62168a6ed5
yo-sa4949 <yosa1129@yahoo.co.jp>
2019-12-23 03:35:25 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_prams)
+    if @user.save
+      flash[:success] = '新規作成に成功しました'
+      redirect_to @user
+    else
+      render :new
+   end
+  end
+  
+  
+  private
+  
+    def user_prams
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
 end
