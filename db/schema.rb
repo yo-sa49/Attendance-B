6d9166562ddeaff1bdcf0dd9488db44fa500014e
yo-sa4949 <yosa1129@yahoo.co.jp>
2019-12-30 06:40:28 +0000

    退勤ボタン追加10.6

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20191229101050) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2019-12-28 23:00:00"
-    t.datetime "work_time", default: "2019-12-28 22:30:00"
+    t.datetime "basic_time", default: "2019-12-29 23:00:00"
+    t.datetime "work_time", default: "2019-12-29 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
