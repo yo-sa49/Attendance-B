3aed0f8e4cdba8a4b94b697e5abf627ed39ee858
yo-sa4949 <yosa1129@yahoo.co.jp>
2020-01-25 05:47:58 +0000

    検索機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :correct_user, only: [:show, :edit, :update]
+  before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
 
   def index
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
 
   def edit
   end
+  
 
   def update
     if @user.update_attributes(user_params)
@@ -57,6 +58,7 @@ class UsersController < ApplicationController
     end
     redirect_to users_url
   end
+  
 
   private
 
@@ -66,6 +68,15 @@ class UsersController < ApplicationController
     
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
+    end
+    
+    # 現在ログインしているユーザーを許可します。
+    def correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end
     end
 
 end
