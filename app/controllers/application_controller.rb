531f7ee6825df74120d37add8615ea613d68ca7b
yo-sa4949 <yosa1129@yahoo.co.jp>
2020-06-21 07:21:33 +0000

    出勤中社員一覧完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  include UsersHelper
+  include AttendancesHelper
 
   $days_of_the_week = %w{日 月 火 水 木 金 土}
   
