330bbe7325d238b991f456f4859af91afc397c7b
yo-sa4949 <yosa1129@yahoo.co.jp>
2020-06-21 15:04:47 +0000

    拠点一覧ページ作成中

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,4 +1,6 @@
 class OfficesController < ApplicationController
   def index
+    @offices = Office.all
+    @office = Office.new
   end
 end
--- app/models/office.rb
+++ app/models/office.rb
@@ -1,2 +1,4 @@
 class Office < ApplicationRecord
+  validates :office_number, presence: true, length: { maximum: 10 }, numericality: { greater_than: 0 }, uniqueness: true
+  validates :office_name, presence: true, length: { maximum: 30 }
 end
